e0c22c8cf7a68ef0b4ebbd1f52ffa7efd8307769
Nitzan Cohen <nitzancohen@hotmail.com>
2023-08-01 12:54:10 +0300

    Improve UI of 404 page

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,24 +1,16 @@
 import './page-not-found.css';
-import { Link } from 'react-router-dom';
+import SecondaryMenu from '../SecondaryMenu/SecondaryMenu';
 
 function PageNotFound(props) {
 
     return(
-        <section className="page-not-found main__content">
-            <h2 className='text__title'>העמוד בבנייה</h2>
-            <p className='text__description'>הגעת לעמוד שעדיין לא מוכן.</p>
-            <p className='text__description'>אנו עובדים במלוא המרץ כדי להגדיל, לשפר ולהוסיף תכנים איכותיים לאתר.</p>
-            <p className='text__description'>בקרוב גם העמוד הזה יהיה מוכן.</p>
-            <ul className="main__menu-list">
-                <li className="main__menu-item">
-                    <Link to={`/`}>
-                        <button 
-                            type="button" 
-                            className="main__menu-button" 
-                            id="coordination-test">חזרה לעמוד הראשי</button>
-                    </Link>
-                </li>
-            </ul>
+        <section className="main__content page-not-found">
+            <SecondaryMenu page="" name="חזרה" />
+            <div className='page404Content'>
+                <h2 className='text__title'>העמוד בבנייה</h2>
+                <p className='text__description'>הגעת לעמוד שעדיין לא מוכן.</p>
+                <p className='text__description'>אנו עובדים במלוא המרץ כדי להגדיל, לשפר ולהוסיף תכנים איכותיים לאתר.</p>
+            </div>
         </section>
     )
 }
